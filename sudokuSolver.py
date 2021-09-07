af48155847b9fa0e9a60beabbaf49c8ebfaf0c62
OwenDelBene <90069108+OwenDelBene@users.noreply.github.com>
2021-09-07 12:47:44 -0500

    Update sudokuSolver.py

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -13,12 +13,15 @@ board = [
 
 
 def solve(b):
+    #look for an empty square
     find = findempty(b)
     if not find:
         return True
     else:
-        row, col =find
+        row, col = find
+    #iterate through possible values
     for i in range(1,10):
+    #Check if they follow the rules of sudoku
         if valid(b, i, (row,col)):
             b[row][col] = i
 
@@ -29,14 +32,16 @@ def solve(b):
 
 
 def valid(b, num, pos):
-    box_x= pos[1] // 3
-    box_y= pos[0] // 3
+    Checking if the proposed value from Solve will follow the rules of sudoku
     for i in range(len(b[0])):
         if b[pos[0]][i] == num and pos[1] != i:
             return False
     for i in range(len(b)):
         if b[i][pos[1]] == num and pos[0] != i:
             return False
+        Creating the Sudoku Boxes
+    box_x= pos[1] // 3
+    box_y= pos[0] // 3
     for i in range(box_y*3, box_y*3 +3):
         for j in range(box_x*3, box_x*3 +3 ):
             if b[i][j] == num and (i,j) != pos:
@@ -46,6 +51,7 @@ def valid(b, num, pos):
     
 
 def print_board(b):
+    #Printing the borders for the Sudoku Boxes
     for i in range(len(b)):
         if i%3 == 0 and i!=0:
             print('------------------')
@@ -57,6 +63,8 @@ def print_board(b):
 
 
 def findempty(b):
+    #iterating through the board and looking for an empty square
+    # 0 means empty
     for i in range(len(b)):
         for j in range(len(b[0])):
             if b[i][j] == 0:
